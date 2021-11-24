883b028814539f09c1fdf7787b610ebad4b9dfbf
Sebastian Rindom <skrindom@gmail.com>
2021-11-24 10:10:41 +0100

    feat: add TaxLine entity

--- packages/medusa/src/models/tax-line.ts
+++ packages/medusa/src/models/tax-line.ts
@@ -1,6 +1,5 @@
 import {
   Entity,
-  OneToMany,
   BeforeInsert,
   CreateDateColumn,
   UpdateDateColumn,
@@ -14,100 +13,30 @@ import {
 import { ulid } from "ulid"
 import { resolveDbType, DbAwareColumn } from "../utils/db-aware-column"
 
-import { TaxLine } from "./tax-line"
-import { Swap } from "./swap"
-import { Cart } from "./cart"
-import { Order } from "./order"
-import { ClaimOrder } from "./claim-order"
-import { ProductVariant } from "./product-variant"
+import { LineItem } from "./line-item"
 
 @Check(`"fulfilled_quantity" <= "quantity"`)
 @Check(`"shipped_quantity" <= "fulfilled_quantity"`)
 @Check(`"returned_quantity" <= "quantity"`)
 @Check(`"quantity" > 0`)
 @Entity()
-export class LineItem {
+export class TaxLine {
   @PrimaryColumn()
   id: string
 
   @Index()
+  @Column()
+  item_id: string
+
+  @ManyToOne(() => LineItem, (li) => li.tax_lines)
+  @JoinColumn({ name: "item_id" })
+  item: LineItem
+
   @Column({ nullable: true })
-  cart_id: string
-
-  @ManyToOne(() => Cart, (cart) => cart.items)
-  @JoinColumn({ name: "cart_id" })
-  cart: Cart
-
-  @Index()
-  @Column({ nullable: true })
-  order_id: string
-
-  @ManyToOne(() => Order, (order) => order.items)
-  @JoinColumn({ name: "order_id" })
-  order: Order
-
-  @Index()
-  @Column({ nullable: true })
-  swap_id: string
-
-  @ManyToOne(() => Swap, (swap) => swap.additional_items)
-  @JoinColumn({ name: "swap_id" })
-  swap: Swap
-
-  @Index()
-  @Column({ nullable: true })
-  claim_order_id: string
-
-  @ManyToOne(() => ClaimOrder, (co) => co.additional_items)
-  @JoinColumn({ name: "claim_order_id" })
-  claim_order: ClaimOrder
-
-  @OneToMany(() => TaxLine, (tl) => tl.item)
-  tax_lines: TaxLine[]
+  rate: string
 
   @Column()
-  title: string
-
-  @Column({ nullable: true })
-  description: string
-
-  @Column({ nullable: true })
-  thumbnail: string
-
-  @Column({ default: false })
-  is_giftcard: boolean
-
-  @Column({ default: true })
-  should_merge: boolean
-
-  @Column({ default: true })
-  allow_discounts: boolean
-
-  @Column({ nullable: true })
-  has_shipping: boolean
-
-  @Column({ type: "int" })
-  unit_price: number
-
-  @Index()
-  @Column({ nullable: true })
-  variant_id: string
-
-  @ManyToOne(() => ProductVariant, { eager: true })
-  @JoinColumn({ name: "variant_id" })
-  variant: ProductVariant
-
-  @Column({ type: "int" })
-  quantity: number
-
-  @Column({ nullable: true, type: "int" })
-  fulfilled_quantity: number
-
-  @Column({ nullable: true, type: "int" })
-  returned_quantity: number
-
-  @Column({ nullable: true, type: "int" })
-  shipped_quantity: number
+  name: string
 
   @CreateDateColumn({ type: resolveDbType("timestamptz") })
   created_at: Date
@@ -118,13 +47,11 @@ export class LineItem {
   @DbAwareColumn({ type: "jsonb", nullable: true })
   metadata: any
 
-  refundable: number | null
-
   @BeforeInsert()
   private beforeInsert() {
     if (this.id) return
     const id = ulid()
-    this.id = `item_${id}`
+    this.id = `tl_${id}`
   }
 }
 
